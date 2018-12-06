f048ebd42776a3e456b1e688e62aa15e4d2c6173
David Simon <D@vidSimon.com>
2018-12-05 22:57:50 -0500

    v0.1 implementation

--- ChildProcess.py
+++ ChildProcess.py
@@ -1,60 +1,163 @@
 import io
 import os
+import signal
 import subprocess
+import sys
+from enum import Enum
 from typing import Dict, List, Tuple, Union
 
-class _Pipe():
-	def __init__(self, read_owner=None, write_owner=None):
-		self._read_owner = read_owner
-		self._write_owner = write_owner
+class ChildProcessIO(Enum):
+	PIPE = 1
+	INHERIT = 2
+	NULL = 3
+	STDOUT = 4
 
-	@property
-	def read_owner(self):
-		return self._read_owner
-
-	@read_owner.setter
-	def read_owner(self, proc):
-		if self.read_owner is not None:
-			raise RuntimeError("Pipe is already being read by another process")
-		elif self.write_owner == proc:
-			raise RuntimeError("A process cannot write to its own input")
-		else:
-			self._read_owner = proc
-
-	@property
-	def write_owner(self):
-		return self._write_owner
-
-	@write_owner.setter
-	def write_owner(self, proc):
-		if self.write_owner is not None:
-			raise RuntimeError("Pipe is already being written to by another process")
-		elif self.read_owner == proc:
-			raise RuntimeError("A process cannot write to its own input")
-		else:
-			self._write_owner = proc
-
-class ProcessIO():
-	STDOUT = subprocess.STDOUT
-	null = os.devnull
-	pass
-
-class Process():
+class ChildProcess():
 	def __init__(self, args, env, cwd, stdin, stdout, stderr):
-		pass
+		self._args = args
+		self._env = env
+		self._cwd = cwd
 
-class ProcessBuilder():
+		popen_stdin, deferred_input = self._make_stdin(stdin)
+		popen_stdout = self._make_stdout(stdout)
+		popen_stderr = self._make_stderr(stderr)
+
+		self._popen = subprocess.Popen(
+			args, cwd=cwd, env=env,
+			stdin=popen_stdin, stdout=popen_stdout, stderr=popen_stderr)
+
+		if deferred_input:
+			self._popen.stdin.write(deferred_input)
+
+	@property
+	def args(self):
+		return self._args
+
+	@property
+	def env(self):
+		return self._env
+
+	@property
+	def cwd(self):
+		return self._cwd
+
+	@property
+	def pid(self):
+		return self._popen.pid
+
+	@property
+	def exit_code(self):
+		return self._popen.poll()
+	
+	@property
+	def stdin(self):
+		if not self._stdin_writeable:
+			raise RuntimeError("The process's stdin pipe is inaccessible")
+		return self._popen.stdin
+
+	@property
+	def stdout(self):
+		if not self._stdin_writeable:
+			raise RuntimeError("The process's stdout pipe is inaccessible")
+		return self._popen.stdout
+
+	@property
+	def stderr(self):
+		if not self._stderr_readable:
+			raise RuntimeError("The process's stderr pipe is inaccessible")
+		return self._popen.stderr
+
+	def _make_stdin(self, stdin):
+		if stdin == ChildProcessIO.PIPE:
+			self._stdin_writeable = True
+			return subprocess.PIPE, None
+		elif stdin == ChildProcessIO.INHERIT:
+			self._stdin_writeable = False
+			return sys.stdin, None
+		elif stdin == ChildProcessIO.NULL:
+			self._stdin_writeable = False
+			return os.devnull, None
+		elif isinstance(stdin, io.IOBase):
+			self._stdin_writeable = False
+			return stdin, None
+		else:
+			self._stdin_writeable = True
+			return subprocess.PIPE, stdin
+
+	def _make_stdout(self, stdout):
+		if stdout == ChildProcessIO.PIPE or stdout == ChildProcessIO.STDOUT:
+			self._stdout_readable = True
+			return subprocess.PIPE
+		elif stdout == ChildProcessIO.INHERIT:
+			self._stdout_readable = False
+			return sys.stdout
+		elif stdout == ChildProcessIO.NULL:
+			self._stdout_readable = False
+			return os.devnull
+		else:
+			self._stdout_readable = False
+			return stdout
+
+	def _make_stderr(self, stderr):
+		if stderr == ChildProcessIO.PIPE:
+			self._stderr_readable = True
+			return subprocess.PIPE
+		elif stderr == ChildProcessIO.STDOUT:
+			self._stderr_readable = False
+			return subprocess.STDOUT
+		elif stderr == ChildProcessIO.INHERIT:
+			self._stderr_readable = False
+			return sys.stderr
+		elif stderr == ChildProcessIO.NULL:
+			self._stderr_readable = False
+			return os.devnull
+		else:
+			self._stderr_readable = False
+			return stderr
+
+	def start(self):
+		if not self.is_stopped():
+			raise RuntimeError("Process is not stopped")
+		self._is_stopped = False
+		self.kill(self.pid, signal.SIGCONT)
+
+	def stop(self):
+		self.kill(self.pid, signal.SIGSTOP)
+		self._is_stopped = True
+
+	def terminate(self, force=False):
+		if force:
+			self._popen.kill()
+		else:
+			self._popen.terminate()
+
+	def is_running(self):
+		return not (self.is_finished() or self.is_stopped())
+
+	def is_stopped(self):
+		return not self.is_finished() and self._is_stopped
+
+	def is_finished(self):
+		return self.exit_code is None
+
+	def wait_for_finish(self, timeout=None):
+		self._popen.wait(timeout)
+
+	def kill(self, signal):
+		self._popen.send_signal(signal)
+
+class ChildProcessBuilder():
 	def __init__(self, args, env=None, cwd=None, stdin=None, stdout=None, stderr=None):
 		if env is None:
 			env = dict(os.environ)
 		if cwd is None:
 			cwd = os.getcwd()
 		if stdin is None:
-			stdin = _Pipe()
+			stdin = ChildProcessIO.PIPE
 		if stdout is None:
-			stdout = _Pipe()
+			stdout = ChildProcessIO.PIPE
 		if stderr is None:
-			stderr = _Pipe()
+			stderr = ChildProcessIO.PIPE
 		self.args = args
 		self.env = env
 		self.cwd = cwd
@@ -66,7 +169,7 @@ class ProcessBuilder():
 		"""
 		Create a child process from the current 
 		"""
-		return Process(self.args, self.env, self.cwd, self.stdin, self.stdout, self.stderr)
+		return ChildProcess(self.args, self.env, self.cwd, self.stdin, self.stdout, self.stderr)
 
 	@property
 	def args(self) -> List[str]:
@@ -119,15 +222,15 @@ class ProcessBuilder():
 
 	@stdin.setter
 	def stdin(self, value):
-		if isinstance(value, str):
+		if isinstance(value, str) or isinstance(value, io.IOBase):
 			self._stdin = value
-		elif isinstance(value, io.IOBase):
-			self._stdin = value
-		elif isinstance(value, _Pipe):
-			value.read_owner = self
-			self._stdin = value
+		elif value in ChildProcessIO:
+			if value == ChildProcessIO.STDOUT:
+				raise ValueError("cannot pipe a process's output to its own input")
+			else:
+				self._stdin = value
 		else:
-			raise TypeError("Input can be redirected from a string, a file-like object, or another process's output")
+			raise TypeError("Input can be redirected from a string, a file-like object, or a ChildProcessIO special value")
 
 	@property
 	def stdout(self):
@@ -135,13 +238,10 @@ class ProcessBuilder():
 
 	@stdout.setter
 	def stdout(self, value):
-		if value == ProcessIO.STDOUT:
-			pass
-		elif isinstance(value, io.IOBase):
+		if isinstance(value, io.IOBase) or value in ChildProcessIO:
 			self._stdout = value
-		elif isinstance(value, _Pipe):
-			value.write_owner = self
-			self._stdout = value
+		else:
+			raise TypeError("Output can be redirected to a file-like object, or a ChildProcessIO special value")
 
 	@property
 	def stderr(self):
@@ -149,10 +249,56 @@ class ProcessBuilder():
 
 	@stderr.setter
 	def stderr(self, value):
-		if value == ProcessIO.STDOUT:
+		if isinstance(value, io.IOBase) or value in ChildProcessIO:
 			self._stderr = value
-		elif isinstance(value, io.IOBase):
-			self._stderr = value
-		elif isinstance(value, _Pipe):
-			value._stderr = self
-			self._stderr = value
+		else:
+			raise TypeError("Error output can be redicted to a file-like object, or a ChildProcessIO special value")
+
+class PipelineBuilder():
+	def __init__(self, commands, env=None, cwd=None, stdin=None, stdout=None, stderr=None):
+		self.commands = commands
+		self.env = env
+		self.cwd = cwd
+		self.stdin = stdin
+		self.stdout = stdout
+		self.stderr = stderr
+		# TODO: make these properly-checked attributes
+
+	def spawnAll(self):
+		res = []
+		next_input = self.stdin
+		builder = ChildProcessBuilder([], env=self.env, cwd=self.cwd, stderr=self.stderr)
+
+		for command in self.commands[:-1]:
+			builder.args = command
+			builder.stdin = next_input
+
+			proc = builder.spawn()
+			res.append(proc)
+			next_input = proc.stdout
+
+		builder.args = commands[-1]
+		builder.stdin = next_input
+		if self.stdout:
+			builder.stdout = self.stdout
+
+		proc = builder.spawn()
+		res.append(proc)
+
+		return res
+
+	@property
+	def commands(self):
+		return self._commands
+	
+	@commands.setter
+	def commands(self, value):
+		if isinstance(value, str):
+			value = value.split("|")
+
+		if isinstance(value, list):
+			if not value:
+				raise ValueError("At least one command must be supplied")
+			self._commands = value
+		else:
+			raise TypeError("The commands should be supplied as a list or as a string with pipe characters")
