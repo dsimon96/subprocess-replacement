57f3b4a90eac3429cef0ec7012b4a493aef6813d
cvsmith <chris@chrissmith.tech>
2018-12-07 10:58:50 -0500

    simple_io runs but crashes on __exit__

--- examples/simple_io.py
+++ examples/simple_io.py
@@ -4,5 +4,5 @@ simple io use case - run another process, send it input, receive output
 from childprocess import ChildProcessBuilder as CPB
 
 with CPB("lua -i -e \"_PROMPT=''\"").spawn() as cp:
-    cp.stdin.write('io.write("Hello world, from ",_VERSION,"!\n")\n')
-    print(cp.stdout.read_line())
+    cp.stdin.write(b'io.write("Hello world, from ",_VERSION,"!\n")\n')
+    print(cp.stdout.readline())
